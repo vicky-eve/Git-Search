ce4f9336636e21d1b11c4d3f7ab7df285f2a4812
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2022-02-25 11:30:43 +0300

    added access token to the environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  accessToken:'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'
 };
 
 /*
