883446c8b22acfca0e25223ce3e1ee21e043b1d3
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2022-02-25 11:52:24 +0300

    added a service to get user details

--- src/app/user-service/user.service.ts
+++ src/app/user-service/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from './user';
-import { Repo } from './repo';
+import { User } from '../user';
+import { Repo } from '../repo';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
