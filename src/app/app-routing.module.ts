3a88f69e463bc3dc22339fada651e0396a191001
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2022-02-25 12:48:18 +0300

    routing structure that links gh username to their repos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UnavailableComponent } from './unavailable/unavailable.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"user-profile", component:UserComponent},
+  {path:"search-repo", component:RepositoryComponent},
+  {path: "", redirectTo:"/user-profile", pathMatch:"full"},
+  {path:"**",component:UnavailableComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
