346959da05ea0670c529c67652b5bf41c6545550
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2022-02-25 12:21:17 +0300

    created a hostlistner decorator for the app in the directive

--- src/app/git.directive.ts
+++ src/app/git.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appGit]'
 })
 export class GitDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) {}
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('green');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 
 }
