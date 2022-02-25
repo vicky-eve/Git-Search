c7306f3bcad5ef8d33a65b0df351e528851fb09e
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2022-02-25 12:27:53 +0300

    created the transform function to allow 5000 requsts per hour with an api key and 60 without one in the pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -1,12 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Repo } from './repo';
 
 @Pipe({
   name: 'dateCount'
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  repos!: Repo;
+  created_at!: Date;
+  transform(value: any): number {
+    let today:Date = new Date(); //get current date and time
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    var dateDifference = Math.abs( todayWithNoTime-value ) //returns value in miliseconds
+    const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
+    var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
+    var roundOff = Math.floor(dateCounter)
+    if (dateCounter >= 1 && todayWithNoTime > value){
+      return roundOff;
+    }else{
+      return 0;
+    }
   }
-
 }
