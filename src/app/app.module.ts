9abfd899871308a912feac52384c749ae8168656
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2022-02-25 11:08:22 +0300

    created components for navbar, user and repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { UserComponent } from './user/user.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoryComponent,
+    UserComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
