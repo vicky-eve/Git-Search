b595e370c2700f54173c8e50da72592963c2ef87
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2022-02-25 11:37:28 +0300

    added a constructor implementation to the user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,12 @@
 export class User {
+    static results: any;
+    constructor ( 
+        public name:string, 
+        public url:string,
+         public login:string, public location:string,
+          public public_repos:number,
+           public followers:number,
+            public  following:number, 
+            public created_at:Date){
+}
 }
