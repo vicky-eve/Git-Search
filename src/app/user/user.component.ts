48bfac86ebf54289038adc99f610b5b8d945ff89
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2022-02-25 12:02:55 +0300

    added a form for user and styling

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Repo } from '../repo';
+import { User } from '../user';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+
+  getUser:any=User;
+      repos:any=Repo;
+
+  constructor(public userService:UserService) {
+  }
+  searchUser(myname:any){
+    this.userService.getDetails(myname).then((success:any)=>{
+      this.getUser = this.userService.gotUser;
+    },
+    (error:any)=>{
+      console.log('enter name')
+    });
+    this.userService.getReDetails(myname).then((success:any)=>{
+      this.repos = this.userService.repos;
+    },
+    (error:any)=>{
+      console.log('repo not found')
+    }
+    );
+  }
 
   ngOnInit(): void {
+    this.searchUser('vicky-eve');
   }
+
+
 
 }
