f85ce129e997ee5800180ecfa85a939fc0d09795
vicky-eve <91916046+vicky-eve@users.noreply.github.com>
2022-02-25 11:43:53 +0300

    added a constructor implementation for the repo class

--- src/app/repo.ts
+++ src/app/repo.ts
@@ -1,2 +1,4 @@
 export class Repo {
+    constructor(public name:string, public login:string, public description:string, public created_at:Date, public forks:number,public watchers_count:number,public followers:number, public following:number, public language:string){
+    }
 }
